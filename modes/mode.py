8a3d6d96306d0a9aa7ae237b9b555a325ab7b28a
tmadi626 <tahaomadi@gmail.com>
2024-02-06 11:08:14 -0330

    Running speech_recognition on a seprate thread. Developed Manual Command switching for testing. Developed unifomed functions & implementation across the modes.

--- modes/mode.py
+++ modes/mode.py
@@ -1,9 +1,17 @@
 class Mode:
+    def __init__(self):
+        self.isActive = None
     
     def activate(self):
         raise NotImplementedError("Subclasses should implement this!")
 
     def deactivate(self):
+        raise NotImplementedError("Subclasses should implement this!")
+
+    def is_active(self) -> bool:
+        return self.isActive
+        
+    def main_loop(self):
         raise NotImplementedError("Subclasses should implement this!")
     
     def __name__(self) -> str:
