2ceb772cc26309dc79b8ad6494af1a68a6adc77c
Sanad Alwerfali <smalwerfali@mun.ca>
2024-02-22 11:53:18 -0330

    modify `control.py` because it was giving me a bug complete `camera.py` with the addition of helper method to notmalize the brightness complete `TextReadingMode` along with its main_loop logic complete `text_recognition' with the addition of helper methods for image processing to easily extract the text

--- modules/text_recognition_io.py
+++ modules/text_recognition_io.py
@@ -1,26 +1,45 @@
 # Text Recognition and Processing Module
-from modules.module__io import ModuleIO
+import cv2
+import numpy as np
+import pytesseract
 
-class TextRecognitionModule(ModuleIO):
+class TextRecognitionModule:
     def __init__(self):
-        # Initialize text recognition parameters
         super().__init__()
+        self.frames_to_wait = 8  
+        self.frame_count = 0
+        self.isEnabled = True
 
-    def recognize_text(self, image):
+    def text_detection(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)
+        se = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
+        dilated = cv2.dilate(binary, se)
+        text = pytesseract.image_to_string(dilated)
+
+        is_text_detected = bool(text.strip())
+        return is_text_detected, text, dilated
+
+    def recognize_text(self, frame):
+        is_detected, detected_text, dilated_frame = self.text_detection(frame)
         if self.isEnabled:
-            # Text recognition logic
-            # Notify the control module with recognition results
-            pass
+            if is_detected:
+                self.frame_count += 1
+
+            if self.frame_count >= self.frames_to_wait:
+                text = detected_text
+
+                self.frame_count = 0
+
+                return text, dilated_frame
+
+        return None, dilated_frame
 
     def enable(self):
-        # Enable text recognition
         self.isEnabled = True
 
     def disable(self):
-        # Disable text recognition
         self.isEnabled = False
 
     def __name__(self) -> str:
         return "TextRecognition"
-
-
