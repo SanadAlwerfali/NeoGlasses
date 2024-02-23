e031b53ca22ec014eb0f82c569b8fb67926098a3
KareemZeair <Kareem_n_z@hotmail.com>
2024-02-23 01:52:57 -0330

    added return statement

--- io_modules/microphone.py
+++ io_modules/microphone.py
@@ -15,26 +15,25 @@ class MicrophoneModule:
             recognizer.adjust_for_ambient_noise(source)
             print("Listening...")
 
-            # Continuous loop to listen for speech input
-            while True:
-                try:
-                    # Listen for speech input from the user
-                    audio_data = recognizer.listen(source)
+            try:
+                # Listen for speech input from the user
+                audio_data = recognizer.listen(source)
 
-                    # Recognize speech using Google Speech Recognition
-                    text = recognizer.recognize_google(audio_data)
+                # Recognize speech using Google Speech Recognition
+                text = recognizer.recognize_google(audio_data)
 
-                    # Print the recognized text in real-time
-                    print("You said:", text)
+                # Print the recognized text in real-time
+                print("You said:", text)
+                return text
 
-                except sr.UnknownValueError:
-                    # Speech not recognized
-                    print("Sorry, could not understand audio")
+            except sr.UnknownValueError:
+                # Speech not recognized
+                print("Sorry, could not understand audio")
 
-                except sr.RequestError as e:
-                    # Unable to access Google Speech Recognition service
-                    print(
-                        "Could not request results from Google Speech Recognition service; {0}".format(
-                            e
-                        )
+            except sr.RequestError as e:
+                # Unable to access Google Speech Recognition service
+                print(
+                    "Could not request results from Google Speech Recognition service; {0}".format(
+                        e
                     )
+                )
